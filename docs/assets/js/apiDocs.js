ab06f641a7ac9278004a999ca6e8bcbdfaf64868
Simon Fricker <simonmfricker@gmail.com>
2017-06-02 13:55:08 +0100

    Styling the docs

--- docs/assets/js/apiDocs.js
+++ docs/assets/js/apiDocs.js
@@ -19,8 +19,8 @@ console.log('apiDocs started');
 $(window).on("load resize scroll",function(e){
 $("#navInner").stick_in_parent();
 
-var widthss = $(".method-example").width();
-$(".examples").width(widthss);
+var widthss = $(".method-example").outerWidth();
+$(".setLang").width(widthss);
 
 
 
@@ -69,3 +69,66 @@ $('a[href*="#"]')
   });
 
   console.log('smoothscroll done');
+
+// Scroll spy - http://jsfiddle.net/mekwall/up4nu/
+  // Cache selectors
+var lastId,
+    topMenu = $(".docsNav"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }
+
+});
+
+
+///load json
+
+$.ajax({
+    url: "apiDocs.json",
+    dataType: "json",
+    success: function(data) {
+        console.log(data);
+        $('body').append(data.employees[0].firstName);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.log('ERROR', textStatus, errorThrown);
+    }
+});
