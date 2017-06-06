98aabd4d83781077c907667d68fab0315320c5dd
Simon Fricker <simonmfricker@gmail.com>
2017-06-06 15:56:48 +0100

    added gumshoe. jQuery issues resolved by vanillas

--- src/js/apiDocs.js
+++ src/js/apiDocs.js
@@ -10,6 +10,7 @@ if($(window).width() > 768)
       $("#navInner").stick_in_parent();
       $(".setLang").stick_in_parent();
     } else {
+      console.log('with < 768')
     }
 
 });
@@ -21,9 +22,9 @@ hljs.initHighlightingOnLoad();
 
 
 
-
-//smoothscroll
-$('a[href*=#]')
+//
+// //smoothscroll
+$('a[href^="#"]')
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
@@ -51,55 +52,58 @@ $('a[href*=#]')
             }
         }
     });
+//
+// // Scroll spy - http://jsfiddle.net/mekwall/up4nu/
+// var lastId,
+//     topMenu = $(".docsNav"),
+//     topMenuHeight = topMenu.outerHeight() + 15,
+//     // All list items
+//     menuItems = topMenu.find("a"),
+//     // Anchors corresponding to menu items
+//     scrollItems = menuItems.map(function() {
+//         var item = $($(this).attr("href"));
+//         if (item.length) {
+//             return item;
+//         }
+//     });
+//
+// // Bind click handler to menu items
+// // so we can get a fancy scroll animation
+// menuItems.click(function(e) {
+//     var href = $(this).attr("href"),
+//         offsetTop = href === '#' ? 0 : $(href).offset().top - topMenuHeight + 1;
+//     $('html, body').stop().animate({
+//         scrollTop: offsetTop
+//     }, 300);
+//     e.preventDefault();
+// });
+//
+// // Bind to scroll
+// $(window).scroll(function() {
+//     // Get container scroll position
+//     var fromTop = $(this).scrollTop() + topMenuHeight;
+//
+//     // Get id of current scroll item
+//     var cur = scrollItems.map(function() {
+//         if ($(this).offset().top < fromTop)
+//             return this;
+//     });
+//     // Get the id of the current element
+//     cur = cur[cur.length - 1];
+//     var id = cur && cur.length ? cur[0].id : "";
+//
+//     if (lastId !== id) {
+//         lastId = id;
+//         // Set/remove active class
+//         menuItems
+//             .parent().removeClass("active")
+//             .end().filter("[href='#" + id + "']").parent().addClass("active");
+//     }
+//
+// });
 
-// Scroll spy - http://jsfiddle.net/mekwall/up4nu/
-var lastId,
-    topMenu = $(".docsNav"),
-    topMenuHeight = topMenu.outerHeight() + 15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function() {
-        var item = $($(this).attr("href"));
-        if (item.length) {
-            return item;
-        }
-    });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e) {
-    var href = $(this).attr("href"),
-        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 300);
-    e.preventDefault();
-});
-
-// Bind to scroll
-$(window).scroll(function() {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop() + topMenuHeight;
-
-    // Get id of current scroll item
-    var cur = scrollItems.map(function() {
-        if ($(this).offset().top < fromTop)
-            return this;
-    });
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    var id = cur && cur.length ? cur[0].id : "";
-
-    if (lastId !== id) {
-        lastId = id;
-        // Set/remove active class
-        menuItems
-            .parent().removeClass("active")
-            .end().filter("[href='#" + id + "']").parent().addClass("active");
-    }
-
-});
+gumshoe.init();
 
 
 
